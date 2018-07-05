d4115d25c689440ce7a9b4e854951d7197e24e3a
Tanner Grehawick <tgrehawi@gmail.com>
2018-07-05 19:32:10 -0400

    minor tweaks

--- Assets/script/util/FPSMonitor.cs
+++ Assets/script/util/FPSMonitor.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class FPSMonitor : MonoBehaviour {
 
   void OnGUI() {
-    GUILayout.Label(string.Format("{0} fps", (int) (1 / Time.unscaledDeltaTime)));
+    float fps;
+    if (Time.timeScale > 0) {
+      fps = 1f / (Time.smoothDeltaTime / Time.timeScale);
+    }
+    else {
+      fps = 1f / Time.unscaledDeltaTime;
+    }
+    GUILayout.Label(string.Format("{0} fps", (int) (fps)));
   }
 }
